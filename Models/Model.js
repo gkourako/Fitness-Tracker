fe0a0fddd84820ec0034091faf5b0505e68d1bcf
gkourako <georgekourakos4@gmail.com>
2020-09-23 19:24:19 -0400

    Initial Commit

---
+++ Models/Model.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose")
+
+const wokroutSchema = new Scehma({
+  name: {
+    type: String,
+    trim: true,
+    required: "Name is required"
+  },
+
+  type: {
+    type: String,
+    trim: true,
+    required: "Type of workout is required"
+  },
+
+  weight: {
+    type: Number,
+    required: "Weight amount is required"
+  },
+
+  sets: {
+    type: Number,
+    required: "Number of sets required"
+  },
+
+  reps: {
+    type: Number,
+    required: "Number of reps required"
+  },
+
+  duration: {
+    type: Number,
+    required: "Duration of workout required"
+  }
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
+
